4e849985beadf76b83a413b2146086b957a4b337
Žiga Potrebuješ <zigapt@gmail.com>
2015-12-17 14:11:28 +0100

    Uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,40 @@
-# 2. faza: Uvoz podatkov
+# 2. FAZA
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
+library(rvest)
+library(dplyr)
+library(gsubfn)
+library(httr)
+library(XML)
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
+naslov1 = "http://www.multpl.com/us-gdp-inflation-adjusted/table"
+gdp <- htmlTreeParse(naslov1, encoding = "UTF-8", useInternal = TRUE)
+gdp <- readHTMLTable(naslov1,which=1)
 
-obcine <- uvozi.obcine()
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+naslov2 = "http://www.multpl.com/us-real-gdp-per-capita/table/by-year"
+gdppc <- htmlTreeParse(naslov2, encoding = "UTF-8", useInternal = TRUE)
+gdppc <- readHTMLTable(naslov2,which=1)
+gdppc <- gdppc[-c(1, 2, 67:nrow(gdppc)),]
+
+
+naslov3 = "http://www.multpl.com/us-real-gdp-growth-rate/table/by-year"
+gr <- htmlTreeParse(naslov3, encoding = "UTF-8", useInternal = TRUE)
+gr <- readHTMLTable(naslov3,which=1)
+
+naslov4 = "http://www.usinflationcalculator.com/inflation/consumer-price-index-and-annual-percent-changes-from-1913-to-2008/"
+cpi <- htmlTreeParse(naslov4, encoding = "UTF-8", useInternal = TRUE)
+cpi <- readHTMLTable(naslov4,which=1)
+cpi <- cpi[-c(1:3, nrow(cpi)), -c(1:13)]
+cpi <- apply(cpi, 2, . %>% as.character(.) %>% as.numeric(.)) %>% data.frame()
+
+naslov5 = "http://www.usinflationcalculator.com/inflation/historical-inflation-rates/"
+usinf <- htmlTreeParse(naslov5, encoding = "UTF-8", useInternal = TRUE)
+usinf <- readHTMLTable(naslov5,which=1)
+usinf <- usinf[c(1, 14), -c(1:37)]
+
+naslov6 = "http://www.multpl.com/unemployment/table"
+unemp <- htmlTreeParse(naslov6, encoding = "UTF-8", useInternal = TRUE)
+unemp <- readHTMLTable(naslov6,which=1)
+
+skupna <- cbind(cpi, cpi, cpi)
+
