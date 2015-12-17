97587e848aef693e964ec1da2019dac8f370e19d
Žiga Potrebuješ <zigapt@gmail.com>
2015-12-17 15:58:02 +0100

    Uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -20,8 +20,8 @@ gdppc <- htmlTreeParse(naslov2, encoding = "UTF-8", useInternal = TRUE)
 gdppc <- readHTMLTable(naslov2,which=1, stringsAsFactors = FALSE)
 gdppc[[1]] <- strapplyc(gdppc[[1]], "([0-9]+)$") %>% as.numeric()
 gdppc[[2]] <- gsub(",", "", gdppc[[2]]) %>% as.numeric()
-colnames(gdppc) <- c("Leto", "BDP p.c. (v $)")
-gdppc <- gdppc %>% filter(Leto < 2015 & Leto >= 1950)
+colnames(gdppc) <- c("Letnica", "BDP p.c. (v $)")
+gdppc <- gdppc %>% filter(Letnica < 2015 & Letnica >= 1950)
 
 
 naslov3 = "http://www.multpl.com/us-real-gdp-growth-rate/table/by-year"
@@ -29,8 +29,8 @@ gr <- htmlTreeParse(naslov3, encoding = "UTF-8", useInternal = TRUE)
 gr <- readHTMLTable(naslov3,which=1, stringsAsFactors = FALSE)
 gr[[1]] <- strapplyc(gr[[1]], "([0-9]+)$") %>% as.numeric()
 gr[[2]] <- strapplyc(gr[[2]], "^([-, 0-9.]+)") %>% as.numeric()
-colnames(gr) <- c("Leto", "Stopnja razvoja")
-gr <- gr %>% filter(Leto <= 2014 & Leto >=1950)
+colnames(gr) <- c("Letnica", "Stopnja razvoja")
+gr <- gr %>% filter(Letnica <= 2014 & Letnica >=1950)
 
 
 naslov4 = "http://www.usinflationcalculator.com/inflation/consumer-price-index-and-annual-percent-changes-from-1913-to-2008/"
@@ -38,19 +38,20 @@ cpi <- htmlTreeParse(naslov4, encoding = "UTF-8", useInternal = TRUE)
 cpi <- readHTMLTable(naslov4,which=1, stringsAsFactors = FALSE)
 cpi <- cpi[-c(1,2), c(1, 14)]
 cpi <- apply(cpi, 1:2, . %>% as.character(.) %>% as.numeric(.)) %>% data.frame()
-colnames(cpi) <- c("Leto", "Indeks cen")
-cpi <- cpi %>% filter(Leto <= 2014 & Leto >= 1950)
-cpi <- cpi %>% arrange(desc(Leto))
+colnames(cpi) <- c("Letnica", "Indeks cen")
+cpi <- cpi %>% filter(Letnica <= 2014 & Letnica >= 1950)
+cpi <- cpi %>% arrange(desc(Letnica))
 
 
 naslov5 = "http://www.usinflationcalculator.com/inflation/historical-inflation-rates/"
 usinf <- htmlTreeParse(naslov5, encoding = "UTF-8", useInternal = TRUE)
 usinf <- readHTMLTable(naslov5,which=1, stringsAsFactors = FALSE)
 usinf <- usinf[-c(1), c(1, 14)]
+usinf[[1]] <- strapplyc(usinf[[1]], "([0-9]+)$") %>% as.numeric()
 usinf[[2]] <- strapplyc(usinf[[2]], "^([-, 0-9.]+)") %>% as.numeric()
-colnames(usinf) <- c("Leto", "Stopnja brezposelnosti")
-usinf <- usinf %>% filter(Leto <= 2014 & Leto >= 1950)
-usinf <- usinf %>% arrange(desc(Leto))
+colnames(usinf) <- c("Letnica", "Stopnja inflacije (v %)")
+usinf <- usinf %>% filter(Letnica <= 2014 & Letnica >= 1950)
+usinf <- usinf %>% arrange(desc(Letnica))
 
 
 naslov6 = "http://www.multpl.com/unemployment/table"
@@ -58,9 +59,9 @@ unemp <- htmlTreeParse(naslov6, encoding = "UTF-8", useInternal = TRUE)
 unemp <- readHTMLTable(naslov6,which=1, stringsAsFactors = FALSE)
 unemp[[1]] <- strapplyc(unemp[[1]], "([0-9]+)$") %>% as.numeric()
 unemp[[2]] <- strapplyc(unemp[[2]], "^([0-9.]+)") %>% as.numeric()
-colnames(unemp) <- c("Leto", "Stopnja brezposelnosti (v %)")
+colnames(unemp) <- c("Letnica", "Stopnja brezposelnosti (v %)")
+unemp <- unemp %>% filter(Letnica <= 2014 & Letnica >= 1950)
 
 
-skupna <- cbind(gdp, gdppc, gr, cpi)
-
+skupna.tabela <- cbind(gdp, gdppc, gr, cpi, usinf, unemp)
 
